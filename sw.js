35e771600d2435b5bafa53ea21fdae0c264040e8
sv55 <sv55@users.noreply.github.com>
2026-06-17 17:06:56 +0530

    version 2.0

---
+++ sw.js
@@ -0,0 +1,36 @@
+var CACHE = "routing-v1";
+var ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./apple-touch-icon.png"
+];
+
+self.addEventListener("install", function(e){
+  e.waitUntil(caches.open(CACHE).then(function(c){ return c.addAll(ASSETS); }));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", function(e){
+  e.waitUntil(
+    caches.keys().then(function(keys){
+      return Promise.all(keys.map(function(k){ if(k !== CACHE) return caches.delete(k); }));
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", function(e){
+  if(e.request.method !== "GET") return;
+  e.respondWith(
+    caches.match(e.request).then(function(hit){
+      return hit || fetch(e.request).then(function(res){
+        var copy = res.clone();
+        caches.open(CACHE).then(function(c){ try{ c.put(e.request, copy); }catch(err){} });
+        return res;
+      }).catch(function(){ return caches.match("./index.html"); });
+    })
+  );
+});
